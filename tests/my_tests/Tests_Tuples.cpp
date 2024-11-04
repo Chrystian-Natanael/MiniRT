9d31923ab45bc8c511c44ca2ad226ede241b65db
cnatanae <cnatanae@student.42sp.org.br>
2024-11-04 18:23:52 -0300

    Testing fail actions

--- tests/my_tests/Tests_Tuples.cpp
+++ tests/my_tests/Tests_Tuples.cpp
@@ -9,7 +9,7 @@ TEST(PointAndVectorTest, TestPointFunction) {
 	double *p = point(1.0, 2.0, 3.0);
 	
 	ASSERT_NE(p, nullptr);
-	EXPECT_DOUBLE_EQ(p[0], 1.0);
+	EXPECT_DOUBLE_EQ(p[0], 2.0);
 	EXPECT_DOUBLE_EQ(p[1], 2.0);
 	EXPECT_DOUBLE_EQ(p[2], 3.0);
 	EXPECT_EQ(static_cast<int>(p[3]), POINT);

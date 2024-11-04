fde2e0273f0d0cc288b7413fa6a9d918d42df8ec
cnatanae <cnatanae@student.42sp.org.br>
2024-11-04 16:29:26 -0300

    Added sum and sub operations

--- includes/Tuples.h
+++ includes/Tuples.h
@@ -2,6 +2,10 @@
 # define TUPLES_H
 
 #include "garbage_collector.h"
+#include <stdbool.h>
+#include <math.h>
+
+#define MAX_DIFF 0.00001
 
 typedef enum e_tuple_type
 {
@@ -11,5 +15,9 @@ typedef enum e_tuple_type
 
 double	*point(double x, double y, double z);
 double	*vector(double x, double y, double z);
+bool	equal(double nbr1, double nbr2);
+
+double	*sum(double *tpl1, double *tpl2);
+double	*sub(double *tpl1, double *tpl2);
 
 #endif // !TUPLES_H

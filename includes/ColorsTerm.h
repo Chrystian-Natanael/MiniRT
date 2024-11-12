5411d85c9c49f40bae48513a99c3e571e0ade7ca
cnatanae <cnatanae@student.42sp.org.br>
2024-11-12 14:30:12 -0300

    Fixing double define name

--- includes/ColorsTerm.h
+++ includes/ColorsTerm.h
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 13:48:16 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/11/11 16:28:08 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:29:02 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 # define RST	"\033[0m"					/* Reset to default color */
 # define RED	"\033[1;31m"				/* Bold Red */
 # define G		"\033[1;32m"				/* Bold Green */
-# define Y		"\033[1;33m"				/* Bold Yellow */
+# define YELLOW	"\033[1;33m"				/* Bold Yellow */
 # define B		"\033[1;34m"				/* Bold Blue */
 # define M		"\033[1;35m"				/* Bold Magenta */
 # define C		"\033[1;36m"				/* Bold Cyan */
-# define W		"\033[1;37m"				/* Bold White */
+# define WHITE	"\033[1;37m"				/* Bold White */
 # define O		"\001\033[38;5;208m\002"	/* Orange */
 
 // Cores em negrito

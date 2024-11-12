8333b89d4c4361ab2c0a871d6743c32b0b1735d0
cnatanae <cnatanae@student.42sp.org.br>
2024-11-12 14:39:39 -0300

    Norminetting

--- includes/Error.h
+++ includes/Error.h
@@ -6,15 +6,15 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:08:29 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/11/12 14:12:37 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:39:05 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
-#include "libft.h"
-#include "garbage_collector.h"
+# include "libft.h"
+# include "garbage_collector.h"
 
 void	warning(char *m1, char *m2, char *m3);
 void	error(char *m1, char *m2, char *m3, int code);
--- includes/MiniRT.h
+++ includes/MiniRT.h
@@ -6,20 +6,20 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:13:31 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/11/12 14:28:16 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:39:18 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-#include "Canvas.h"
-#include "Colors.h"
-#include "ColorsTerm.h"
-#include "Error.h"
-#include "Tuples.h"
-#include "MLX42/MLX42.h"
-#include "garbage_collector.h"
-#include "libft.h"
+# include "Canvas.h"
+# include "Colors.h"
+# include "ColorsTerm.h"
+# include "Error.h"
+# include "Tuples.h"
+# include "MLX42/MLX42.h"
+# include "garbage_collector.h"
+# include "libft.h"
 
 #endif // ! MINIRT_H

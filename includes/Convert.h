febf84e045c9a2dc0d795f88ee915ce561f40fe4
cnatanae <cnatanae@student.42sp.org.br>
2024-11-14 09:31:27 -0300

    :bug: fix: Added lib math

--- includes/Convert.h
+++ includes/Convert.h
@@ -6,12 +6,14 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:22:24 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/11/12 16:23:06 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/11/14 08:43:31 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONVERT_H
 # define CONVERT_H
+
+# include <math.h>
 
 int	double_to_int(double value);
 

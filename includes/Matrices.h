c015c082df8e46ec5185b604811ae1828cb8f549
Thais Malheiros Assumpcao <tmalheir@c2r8p3.42sp.org.br>
2024-11-14 11:18:54 -0300

    :sparkles: feat: matrix_create and matrix_compare

--- includes/Matrices.h
+++ includes/Matrices.h
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MiniRT.h                                           :+:      :+:    :+:   */
+/*   Matrices.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 14:13:31 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/11/14 10:34:55 by tmalheir         ###   ########.fr       */
+/*   Created: 2024/11/14 09:40:12 by tmalheir          #+#    #+#             */
+/*   Updated: 2024/11/14 11:04:41 by tmalheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINIRT_H
-# define MINIRT_H
+#ifndef MATRICES_H
+# define MATRICES_H
 
-# include "Canvas.h"
-# include "Colors.h"
-# include "ColorsTerm.h"
+# include <stdbool.h>
 # include "Error.h"
-# include "Matrices.h"
 # include "Tuples.h"
-# include "MLX42/MLX42.h"
-# include "garbage_collector.h"
-# include "libft.h"
 
-#endif // ! MINIRT_H
+typedef struct	s_matrix
+{
+	int		row;
+	int		col;
+	double	content[16];
+}		t_matrix;
+
+t_matrix	create_matrix(int row, int col, double *elements);
+bool		compare_matrix(t_matrix mtx_a, t_matrix mtx_b);
+
+#endif // ! MATRICES_H
---
+++ src/Matrices/Matrices.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Matrices.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 09:43:54 by tmalheir          #+#    #+#             */
+/*   Updated: 2024/11/14 11:08:29 by tmalheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Matrices.h"
+
+t_matrix	create_matrix(int row, int col, double *elements)
+{
+	int			ir;
+	int			ic;
+	t_matrix	matrix;
+
+	ft_bzero(&matrix, sizeof(t_matrix));
+	if (!elements)
+		error("empty elements", NULL, NULL, 1);
+	if (row < 1 || col < 1)
+		error("Rows and cols cannot be lesser than one", NULL, NULL, 1);
+	ir = -1;
+	matrix.row = row;
+	matrix.col = col;
+	while (++ir < row)
+	{
+		ic = -1;
+		while (++ic < col)
+		{
+			matrix.content[ir * col + ic] = *elements;
+			elements++;
+		}
+	}
+	return (matrix);
+}
+
+bool	compare_matrix(t_matrix mtx_a, t_matrix mtx_b)
+{
+	int	ir;
+	int	ic;
+	int	pos;
+
+	if (mtx_a.row != mtx_b.row || mtx_a.col != mtx_b.col)
+		return (false);
+	ir = -1;
+	while (++ir < mtx_a.row)
+	{
+		ic = -1;
+		while (++ic < mtx_a.col)
+		{
+			pos = ir * mtx_a.col + ic;
+			if (!equal(mtx_a.content[pos], mtx_b.content[pos]))
+				return (false);
+		}
+	}
+	return (true);
+}

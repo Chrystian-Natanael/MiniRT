4cf53b86f1df9d4b202a978f0aa3e27efe82b71e
cnatanae <cnatanae@student.42sp.org.br>
2024-11-14 09:32:11 -0300

    :test_tybe: test: Added test to function of convert double to int

---
+++ tests/my_tests/Tests_Conversion.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <cmath>
+
+extern "C" {
+	#include "Convert.h"
+}
+
+TEST(DoubleToIntTest, PositiveValues) {
+	EXPECT_EQ(double_to_int(2.3), 2);
+	EXPECT_EQ(double_to_int(2.5), 3);
+	EXPECT_EQ(double_to_int(2.7), 3);
+}
+
+TEST(DoubleToIntTest, NegativeValues) {
+	EXPECT_EQ(double_to_int(-2.3), -2);
+	EXPECT_EQ(double_to_int(-2.5), -3);
+	EXPECT_EQ(double_to_int(-2.7), -3);
+}
+
+TEST(DoubleToIntTest, ZeroValue) {
+	EXPECT_EQ(double_to_int(0.0), 0);
+}
+
+TEST(DoubleToIntTest, BoundaryValues) {
+	EXPECT_EQ(double_to_int(1.499999), 1);
+	EXPECT_EQ(double_to_int(1.500001), 2);
+	EXPECT_EQ(double_to_int(-1.499999), -1);
+	EXPECT_EQ(double_to_int(-1.500001), -2);
+}

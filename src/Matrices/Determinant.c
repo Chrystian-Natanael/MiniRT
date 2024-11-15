fc487a71306325703ca279ffd2e1851b4344c31d
cnatanae <cnatanae@student.42sp.org.br>
2024-11-15 20:02:10 -0300

    :recycle: refactor: Added verification of type of determinant

--- src/Matrices/Determinant.c
+++ src/Matrices/Determinant.c
@@ -6,14 +6,15 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:00:27 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/11/15 14:12:21 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/11/15 20:00:16 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Matrices.h"
 
-double	determinant(t_matrix matrix)
-{
+static double	determinant_2x2(t_matrix matrix);
+
+static double	determinant_2x2(t_matrix matrix){
 	double	el_a;
 	double	el_b;
 	double	el_c;
@@ -24,4 +25,12 @@ double	determinant(t_matrix matrix)
 	el_c = get_value_pos(1, 0, matrix);
 	el_d = get_value_pos(1, 1, matrix);
 	return ((el_a * el_d) - (el_b * el_c));
+}
+
+double	determinant(t_matrix matrix)
+{
+	if (matrix.col == 2 && matrix.row == 2)
+		return (determinant_2x2(matrix));
+	// if (matrix.col == 3 && matrix.row == 3)
+		// return (determinant_3x3(matrix));
 }

53659cc26407ab590ee858a4dd3e7128666461e1
cnatanae <cnatanae@student.42sp.org.br>
2024-11-20 16:29:14 -0300

    :sparkles: feat: Added function to create a translation matrix

---
+++ src/Matrices/Translation.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Translation.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 12:54:32 by cnatanae          #+#    #+#             */
+/*   Updated: 2024/11/20 14:16:15 by cnatanae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Matrices.h"
+
+t_matrix	translation(double x, double y, double z)
+{
+	t_matrix	trnsl_mtx;
+
+	trnsl_mtx = identity();
+	set_value_pos((get_pos(0, 3, 4)), x, &trnsl_mtx);
+	set_value_pos((get_pos(1, 3, 4)), y, &trnsl_mtx);
+	set_value_pos((get_pos(2, 3, 4)), z, &trnsl_mtx);
+	return (trnsl_mtx);
+}

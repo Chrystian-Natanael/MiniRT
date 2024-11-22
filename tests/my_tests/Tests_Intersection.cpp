80e476cf54b8bad44e8c65bc827fc2b00142919d
cnatanae <cnatanae@student.42sp.org.br>
2024-11-22 11:13:32 -0300

    :test_tube: test: Create a file and first test to intersections

---
+++ tests/my_tests/Tests_Intersection.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <cmath>
+
+extern "C" {
+	#include "Utils.h"
+	#include "Intersections.h"
+}
+
+TEST(TesterRay, CreateAndQueryRay) {
+	double *origin = point(1, 2, 3);
+	double *direction = vector(4, 5, 6);
+	t_ray r = ray(origin, direction);
+
+	ASSERT_NE(r.origin, nullptr);
+	ASSERT_NE(r.direction, nullptr);
+
+	EXPECT_TRUE(equal(r.origin[0], origin[0]));
+	EXPECT_TRUE(equal(r.origin[1], origin[1]));
+	EXPECT_TRUE(equal(r.origin[2], origin[2]));
+	EXPECT_TRUE(equal(r.origin[3], origin[3]));
+
+	EXPECT_TRUE(equal(r.direction[0], direction[0]));
+	EXPECT_TRUE(equal(r.direction[1], direction[1]));
+	EXPECT_TRUE(equal(r.direction[2], direction[2]));
+	EXPECT_TRUE(equal(r.direction[3], direction[3]));
+
+	free(origin);
+	free(direction);
+}

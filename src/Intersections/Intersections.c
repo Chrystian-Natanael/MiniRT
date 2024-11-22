6696288c4b6c9652610517ff460084296b689d79
Thais Malheiros Assumpcao <tmalheir@c2r4p5.42sp.org.br>
2024-11-22 12:27:21 -0300

    :sparkles: feat: intersections -> ray position

--- src/Intersections/Intersections.c
+++ src/Intersections/Intersections.c
@@ -1,31 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Intersections.h                                    :+:      :+:    :+:   */
+/*   Intersections.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/22 11:06:07 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/11/22 12:15:33 by tmalheir         ###   ########.fr       */
+/*   Created: 2024/11/22 12:16:17 by tmalheir          #+#    #+#             */
+/*   Updated: 2024/11/22 12:19:11 by tmalheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INTERSECTIONS_H
-# define INTERSECTIONS_H
+#include "Intersections.h"
+#include "Sphere.h"
 
-typedef struct s_ray
+t_intersec	intersect(t_sphere *sphere, t_ray ray)
 {
-	double	*origin;
-	double	*direction;
-}			t_ray;
-
-typedef struct s_intersec
-{
-	int		count;
-	double	*intersections;
-}			t_intersec;
-
-t_ray	create_ray(double *origin, double *direction);
-double	*pos_ray(t_ray ray, double time);
-
-#endif
+	if (!sphere)
+		error("Error\n", "Sphere doesn't exist", NULL, ERROR);
+}

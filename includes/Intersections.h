b2852445356c9a0ba3ea5e01e6e43e874f498d8f
cnatanae <cnatanae@student.42sp.org.br>
2024-11-22 13:17:39 -0300

    :sparkles: feat: Finish intersect function

--- includes/Intersections.h
+++ includes/Intersections.h
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   Intersections.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 11:06:07 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/11/22 12:15:33 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/11/22 13:11:13 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERSECTIONS_H
 # define INTERSECTIONS_H
+
+# include "Sphere.h"
 
 typedef struct s_ray
 {
@@ -25,7 +27,17 @@ typedef struct s_intersec
 	double	*intersections;
 }			t_intersec;
 
-t_ray	create_ray(double *origin, double *direction);
-double	*pos_ray(t_ray ray, double time);
+typedef struct s_coef
+{
+	double	a;
+	double	b;
+	double	c;
+	double	discrim;
+}			t_coef;
+
+t_ray		create_ray(double *origin, double *direction);
+double		*pos_ray(t_ray ray, double time);
+
+t_intersec	*intersect(t_sphere *sphere, t_ray ray);
 
 #endif
--- src/Intersections/Intersections.c
+++ src/Intersections/Intersections.c
@@ -3,18 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   Intersections.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 12:16:17 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/11/22 12:19:11 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/11/22 13:09:30 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intersections.h"
-#include "Sphere.h"
 
-t_intersec	intersect(t_sphere *sphere, t_ray ray)
+static t_intersec	*calc_intersection(t_coef coef)
 {
+	t_intersec	*list;
+
+	list = allocate(sizeof(t_intersec));
+	if (coef.discrim < 0)
+		return (list);
+	list->intersections = allocate(sizeof(double) * 2);
+	list->count = 2;
+	list->intersections[0] = ((coef.b * -1) - sqrt(coef.discrim)) / (2
+			* coef.a);
+	list->intersections[1] = ((coef.b * -1) + sqrt(coef.discrim)) / (2
+			* coef.a);
+	return (list);
+}
+
+static t_coef	calc_coef(t_sphere *sphere, t_ray ray)
+{
+	t_coef	coef;
+	double	*sphere_to_ray;
+
+	sphere_to_ray = sub(ray.origin, sphere->origin);
+	coef.a = dot_prod(ray.direction, ray.direction);
+	coef.b = 2 * dot_prod(ray.direction, sphere_to_ray);
+	coef.c = dot_prod(sphere_to_ray, sphere_to_ray) - 1;
+	coef.discrim = pow(coef.b, 2) - 4 * coef.a * coef.c;
+	return (coef);
+}
+
+t_intersec	*intersect(t_sphere *sphere, t_ray ray)
+{
+	t_coef	coef;
+
 	if (!sphere)
 		error("Error\n", "Sphere doesn't exist", NULL, ERROR);
+	coef = calc_coef(sphere, ray);
+	return (calc_intersection(coef));
 }

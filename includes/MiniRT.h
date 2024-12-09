09c21df0d9edfc0feef9ff30e09bca04b714044d
Chrystian Natanael <chrystian.natanael.msn@gmail.com>
2024-12-09 12:00:26 -0300

    :refactor: refactor: Updated all includes

--- includes/MiniRT.h
+++ includes/MiniRT.h
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:13:31 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/12/06 10:30:57 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/12/09 11:52:43 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 # include "garbage_collector.h"
 # include "Sphere.h"
 # include "Tuples.h"
+# include "Light_and_Shading.h"
 # include "MLX42/MLX42.h"
 
 #endif // ! MINIRT_H

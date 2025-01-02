be838f51f7e341a0d2ccdaccd0a98d0f496a0b7d
cnatanae <cnatanae@student.42sp.org.br>
2025-01-02 11:07:00 -0300

    :recycle: refactor: Create a new define for only tests compilation

--- includes/Mem_pool.h
+++ includes/Mem_pool.h
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 10:25:51 by cnatanae          #+#    #+#             */
-/*   Updated: 2025/01/02 11:01:58 by cnatanae         ###   ########.fr       */
+/*   Updated: 2025/01/02 11:06:38 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include <stdio.h>
 
-// # define MAX_MEM 5000000000
-# define MAX_MEM 5000
+// # define MAX_MEM 5000000000 // for grafic
+# define MAX_MEM 5000 // for tests
 
 typedef struct s_pool
 {

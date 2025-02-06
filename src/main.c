09387e04cfc690908682b9e716ddcd95bc8afa4c
Thais Malheiros Assumpcao <tmalheir@c1r7p1.42sp.org.br>
2025-02-06 15:46:22 -0300

    :sparkles: feat: main with saving file option

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:58:24 by cnatanae          #+#    #+#             */
-/*   Updated: 2025/02/06 15:18:47 by cnatanae         ###   ########.fr       */
+/*   Updated: 2025/02/06 15:37:42 by tmalheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ int	main(int argc, char **argv)
 
 	world = parser(argc, argv);
 	paint = render_canva(world->camera, world);
+	if (world->scene.save_img)
+		canva2ppm(paint, world->scene.file_name);
 	mlx_set_setting(MLX_STRETCH_IMAGE, true);
-	mlx = mlx_init(world->camera.hsize, world->camera.vsize, "teste", true);
+	mlx = mlx_init(world->camera.hsize, world->camera.vsize, "miniRT", true);
 	image = canva2image(paint, mlx);
 	mlx_image_to_window(mlx, image, 0, 0);
 	mlx_loop(mlx);

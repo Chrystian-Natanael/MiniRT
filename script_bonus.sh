d539f565f93b358ba5e310ec067f0965d0d7e8f5
cnatanae <cnatanae@student.42sp.org.br>
2025-02-10 12:23:12 -0300

    :sparkles: feat: Added scrips to avaliete leaks

---
+++ script_bonus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+output_file="out_bonus.txt"
+
+> "$output_file"
+
+directories=("scenes/error" "scenes/bonus" "scenes/basic")
+
+for dir in "${directories[@]}"; do
+	for map in "$dir"/*; do
+		valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes --suppressions=.suppress_mlx_error.sup ./bin/miniRT_bonus "$map" >> "$output_file" 2>&1
+	done
+done
+
+sed -i '/^$/d' "$output_file"
+sed -i '/^\[uProgress:/d' "$output_file"
